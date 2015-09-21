c5739f3c09d9027fe9c6a1261658046bf15e5d0d
jeanpaolopacana <jeanpaolopacana@yahoo.com>
2015-09-21 19:38:50 +0800

    update woocommerce

--- shop/wp-content/plugins/woocommerce/assets/js/frontend/cart-fragments.js
+++ shop/wp-content/plugins/woocommerce/assets/js/frontend/cart-fragments.js
@@ -1,60 +1,75 @@
-jQuery(document).ready(function($) {
+/* global wc_cart_fragments_params */
+jQuery( function( $ ) {
 
-	/** Cart Handling */
-	$supports_html5_storage = ( 'sessionStorage' in window && window['sessionStorage'] !== null );
+	// wc_cart_fragments_params is required to continue, ensure the object exists
+	if ( typeof wc_cart_fragments_params === 'undefined' ) {
+		return false;
+	}
 
-	$fragment_refresh = {
-		url: woocommerce_params.ajax_url,
+	/* Storage Handling */
+	var $supports_html5_storage;
+	try {
+		$supports_html5_storage = ( 'sessionStorage' in window && window.sessionStorage !== null );
+
+		window.sessionStorage.setItem( 'wc', 'test' );
+		window.sessionStorage.removeItem( 'wc' );
+	} catch( err ) {
+		$supports_html5_storage = false;
+	}
+
+	var $fragment_refresh = {
+		url: wc_cart_fragments_params.wc_ajax_url.toString().replace( '%%endpoint%%', 'get_refreshed_fragments' ),
 		type: 'POST',
-		data: { action: 'woocommerce_get_refreshed_fragments' },
 		success: function( data ) {
 			if ( data && data.fragments ) {
 
 				$.each( data.fragments, function( key, value ) {
-					$(key).replaceWith(value);
+					$( key ).replaceWith( value );
 				});
 
 				if ( $supports_html5_storage ) {
-					sessionStorage.setItem( "wc_fragments", JSON.stringify( data.fragments ) );
-					sessionStorage.setItem( "wc_cart_hash", data.cart_hash );
+					sessionStorage.setItem( wc_cart_fragments_params.fragment_name, JSON.stringify( data.fragments ) );
+					sessionStorage.setItem( 'wc_cart_hash', data.cart_hash );
 				}
 
-				$('body').trigger( 'wc_fragments_refreshed' );
+				$( document.body ).trigger( 'wc_fragments_refreshed' );
 			}
 		}
 	};
 
+	/* Cart Handling */
 	if ( $supports_html5_storage ) {
 
-		$('body').bind( 'added_to_cart', function( event, fragments, cart_hash ) {
-			sessionStorage.setItem( "wc_fragments", JSON.stringify( fragments ) );
-			sessionStorage.setItem( "wc_cart_hash", cart_hash );
+		$( document.body ).bind( 'added_to_cart', function( event, fragments, cart_hash ) {
+			sessionStorage.setItem( wc_cart_fragments_params.fragment_name, JSON.stringify( fragments ) );
+			sessionStorage.setItem( 'wc_cart_hash', cart_hash );
 		});
 
 		try {
-			var wc_fragments = $.parseJSON( sessionStorage.getItem( "wc_fragments" ) );
-			var cart_hash    = sessionStorage.getItem( "wc_cart_hash" );
-			var cookie_hash  = $.cookie( "woocommerce_cart_hash" );
+			var wc_fragments = $.parseJSON( sessionStorage.getItem( wc_cart_fragments_params.fragment_name ) ),
+				cart_hash    = sessionStorage.getItem( 'wc_cart_hash' ),
+				cookie_hash  = $.cookie( 'woocommerce_cart_hash' );
 
-			if ( cart_hash == null || cart_hash == undefined || cart_hash == '' )
+			if ( cart_hash === null || cart_hash === undefined || cart_hash === '' ) {
 				cart_hash = '';
-
-			if ( cookie_hash == null || cookie_hash == undefined || cookie_hash == '' )
-				cookie_hash = '';
-
-			if ( wc_fragments && wc_fragments['div.widget_shopping_cart_content'] && cart_hash == cookie_hash ) {
-
-				$.each( wc_fragments, function( key, value ) {
-					$(key).replaceWith(value);
-				});
-
-				$('body').trigger( 'wc_fragments_loaded' );
-
-			} else {
-				throw "No fragment";
 			}
 
-		} catch(err) {
+			if ( cookie_hash === null || cookie_hash === undefined || cookie_hash === '' ) {
+				cookie_hash = '';
+			}
+
+			if ( wc_fragments && wc_fragments['div.widget_shopping_cart_content'] && cart_hash === cookie_hash ) {
+
+				$.each( wc_fragments, function( key, value ) {
+					$( key ).replaceWith(value);
+				});
+
+				$( document.body ).trigger( 'wc_fragments_loaded' );
+			} else {
+				throw 'No fragment';
+			}
+
+		} catch( err ) {
 			$.ajax( $fragment_refresh );
 		}
 
@@ -62,14 +77,14 @@ jQuery(document).ready(function($) {
 		$.ajax( $fragment_refresh );
 	}
 
-	/* Cart hiding */
-	if ( $.cookie( "woocommerce_items_in_cart" ) > 0 )
-		$('.hide_cart_widget_if_empty').closest('.widget_shopping_cart').show();
-	else
-		$('.hide_cart_widget_if_empty').closest('.widget_shopping_cart').hide();
+	/* Cart Hiding */
+	if ( $.cookie( 'woocommerce_items_in_cart' ) > 0 ) {
+		$( '.hide_cart_widget_if_empty' ).closest( '.widget_shopping_cart' ).show();
+	} else {
+		$( '.hide_cart_widget_if_empty' ).closest( '.widget_shopping_cart' ).hide();
+	}
 
-	$('body').bind( 'adding_to_cart', function() {
-		$('.hide_cart_widget_if_empty').closest('.widget_shopping_cart').show();
-	} );
-
+	$( document.body ).bind( 'adding_to_cart', function() {
+		$( '.hide_cart_widget_if_empty' ).closest( '.widget_shopping_cart' ).show();
+	});
 });
