a265eded574319260c4924612e5f072b1dcecd5e
jeromemisfit <jerome@misfit-inc.com>
2016-01-13 19:23:55 +0800

    shopkeeper theme: modifications
    - functions.php: disabled review tab and removed related products below
    the single product section but above the footer.
    - product-attributes.php: added a new file. added new block for
    shortcode post_view counter

--- shop/wp-content/themes/shopkeeper/functions.php
+++ shop/wp-content/themes/shopkeeper/functions.php
@@ -814,3 +814,23 @@ if ( ! function_exists('shopkeeper_woocommerce_image_dimensions') ) :
 endif;
 
 if ( ! isset( $content_width ) ) $content_width = 900;
+
+
+
+/******************************************************************************/
+/****** CUSTOM ******************************************/
+/******************************************************************************/
+
+/* disable review tab on product pages */
+add_filter( 'woocommerce_product_tabs', 'wcs_woo_remove_reviews_tab', 98 );
+function wcs_woo_remove_reviews_tab($tabs) {
+	unset($tabs['reviews']);
+	return $tabs;
+}
+
+
+/* Remove Related Products Output */
+function wc_remove_related_products( $args ) {
+	return array();
+}
+add_filter('woocommerce_related_products_args','wc_remove_related_products', 10); 
